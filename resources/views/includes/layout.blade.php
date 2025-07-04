b984688f72aa2e56d42dad77b6314fe56a00f7bb
Shashikanta Das <shashikantadas458@gmail.com>
2025-07-04 23:22:27 +0530

    some changes

--- resources/views/includes/layout.blade.php
+++ resources/views/includes/layout.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="{{ asset('images/iig_logo.png') }}">
+    <link rel="icon" href="">
 
     <title>Admin - Dashboard</title>
 
